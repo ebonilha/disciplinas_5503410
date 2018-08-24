86c1a0ef095e9adc0358a247a6ef03b99583bec6
ebonilha <ebonilha@usp.br>
2018-08-24 14:45:53 -0300

    Issue 4 - Form Cadastro de Disciplina

---
+++ resources/views/disciplinas/create.blade.php
@@ -0,0 +1,6 @@
+<form method="POST" action="/disciplinas">
+    {{ csrf_field() }}
+    Nome: <input name="titulo">
+    <p>Ementa: <textarea name="ementa"> </textarea></p>
+    <button type="submit"> Salvar </button>
+</form>
