c72a22bc3878f384c92f97a7a594370731d88759
ebonilha <ebonilha@usp.br>
2018-09-03 09:08:00 -0300

    Issue 10 - Exibe as turmas da disciplina

--- resources/views/disciplinas/show.blade.php
+++ resources/views/disciplinas/show.blade.php
@@ -2,5 +2,10 @@
 <p>{{ $disciplina->ementa }}</p>
 
 <a href="/disciplinas/{{ $disciplina->id }}/turmas/create">Criar Turma</a>
-
+<p>
+@foreach ($disciplina->turmas as $turma)
+	{{ $turma->ministrante }}<br>
+	{{ $turma->inicio }}<br>
+@endforeach
+</p>
 <a href="/"> Voltar </a>
